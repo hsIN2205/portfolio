bca69a389d7b0913a7aa36c061bbb6a6d92a6cdd
Inchijeong <cde0409@naver.com>
2020-12-18 01:05:27 +0900

    build

---
+++ precache-manifest.66cb47dc2443395567c2fab25e0abaac.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fab8975177947df21a7e89d5ff20360d",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "a6684be23d7e16fcf61d",
+    "url": "/portfolio/static/css/2.ca1e55e4.chunk.css"
+  },
+  {
+    "revision": "2d36dd5b61b74bd2eb11",
+    "url": "/portfolio/static/css/main.db02b48a.chunk.css"
+  },
+  {
+    "revision": "a6684be23d7e16fcf61d",
+    "url": "/portfolio/static/js/2.3ba40c49.chunk.js"
+  },
+  {
+    "revision": "2d36dd5b61b74bd2eb11",
+    "url": "/portfolio/static/js/main.fdd94106.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "f91a9541d7bf536930f103f14109b4f0",
+    "url": "/portfolio/static/media/bg.f91a9541.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "1f5ea703debf6bf580eb137a8e319a79",
+    "url": "/portfolio/static/media/icon-work1.1f5ea703.svg"
+  },
+  {
+    "revision": "60b056c843fd0a66a72e15ab15e9e0b0",
+    "url": "/portfolio/static/media/icon-work2.60b056c8.svg"
+  },
+  {
+    "revision": "7c441afcd5993fdaba45937b7a45ee9f",
+    "url": "/portfolio/static/media/icon-work3.7c441afc.svg"
+  }
+]);
