1139cd971fa5ad1603f0f1987259e15f06771132
지유 <jiyu@jiyuui-MacBookPro.local>
2020-12-19 01:03:38 +0900

    Updates

---
+++ precache-manifest.2d3657de14d956478cb03de856f33174.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4edc70bdf76053bb8a47fb4d7f0390f1",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "9fa85f628061c1524727",
+    "url": "/portfolio/static/css/2.5d7e6c6f.chunk.css"
+  },
+  {
+    "revision": "e3f4854a00d25b1a9395",
+    "url": "/portfolio/static/css/main.e62991c5.chunk.css"
+  },
+  {
+    "revision": "9fa85f628061c1524727",
+    "url": "/portfolio/static/js/2.ee65ac3b.chunk.js"
+  },
+  {
+    "revision": "e3f4854a00d25b1a9395",
+    "url": "/portfolio/static/js/main.c604e9e2.chunk.js"
+  },
+  {
+    "revision": "53ebc025fca807d4dbea",
+    "url": "/portfolio/static/js/runtime~main.f308c8e6.js"
+  },
+  {
+    "revision": "f91a9541d7bf536930f103f14109b4f0",
+    "url": "/portfolio/static/media/bg.f91a9541.jpg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/portfolio/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/portfolio/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/portfolio/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/portfolio/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/portfolio/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "a754b9b2766020da8753e678a8aa8084",
+    "url": "/portfolio/static/media/icon-work1.a754b9b2.svg"
+  },
+  {
+    "revision": "bfcea0e5d438346449592263b81f8458",
+    "url": "/portfolio/static/media/icon-work2.bfcea0e5.svg"
+  },
+  {
+    "revision": "1f46953fded33aa8c82fa4d0886f3800",
+    "url": "/portfolio/static/media/icon-work3.1f46953f.svg"
+  }
+]);
